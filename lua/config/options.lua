f3c2e1926073c879374ad2a9e53844822edc5f90
tpregier <horseflie93@gmail.com>
2025-09-08 16:43:26 -0600

    updated options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-    -- In your config/options.lua file
+-- In your config/options.lua file
+
+vim.opt.wrap = true
+vim.opt.textwidth = 80
