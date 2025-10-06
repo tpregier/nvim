285d1c5f21f4402964d5914ac73c8afad85ec380
tpregier <horseflie93@gmail.com>
2025-10-06 14:57:38 -0600

    changes to highlight-undo

--- lua/plugins/highlight-undo.lua
+++ lua/plugins/highlight-undo.lua
@@ -1,12 +1,30 @@
 return {
   "tzachar/highlight-undo.nvim",
-  opts = {
-    hlgroup = "HighlightUndo",
-    duration = 300,
-    pattern = { "*" },
-    ignored_filetypes = { "neo-tree", "fugitive", "TelescopePrompt", "mason", "lazy" },
-    -- ignore_cb is in comma as there is a default implementation. Setting
-    -- to nil will mean no default os called.
-    -- ignore_cb = nil,
-  },
+  event = "BufRead",
+  config = function()
+    require("highlight-undo").setup({
+      hlgroup = "HighlightUndo",
+      duration = 1000, -- stays visible for 2 seconds
+    })
+
+    -- function to pick a visible color depending on background
+    local function set_dynamic_highlight()
+      local bg = vim.o.background
+      if bg == "dark" then
+        -- Bright color for dark themes
+        vim.api.nvim_set_hl(0, "HighlightUndo", { bg = "#ff5555", fg = "#ffffff", bold = true })
+      else
+        -- Darker, but still visible color for light themes
+        vim.api.nvim_set_hl(0, "HighlightUndo", { bg = "#ff0000", fg = "#000000", bold = true })
+      end
+    end
+
+    -- Set it immediately
+    set_dynamic_highlight()
+
+    -- Reapply when colorscheme changes
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = set_dynamic_highlight,
+    })
+  end,
 }
